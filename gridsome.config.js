a290236302b20bc7327ba6fca0122f8faebf4a62
Benoit DAUBA <benoit@dauba.net>
2019-11-20 17:12:50 +0100

    Description gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -14,9 +14,9 @@ const postcssPlugins = [
 if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss())
 
 module.exports = {
-  siteName: 'Gridsome Portfolio Starter',
-  siteDescription: 'A simple portfolio theme for Gridsome powered by Tailwind CSS v1',
-  siteUrl: 'https://gridsome-portfolio-starter.netlify.com',
+  siteName: 'Benoit Dauba - Développeur, Intégrateur Web Freelance',
+  siteDescription: 'Le Blog de Benoit Dauba',
+  siteUrl: 'https://blog.dauba.net',
   plugins: [
     {
       use: '@gridsome/vue-remark',
